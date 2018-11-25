ecf6eab9ed0acf8dae49f5839cc6decc40c8fbf5
Matteo <matteodocci.dev@gmail.com>
2018-11-25 15:34:41 +0100

    Add Simple JWT.

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# Pagination
+# REST FRAMEWORK
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
